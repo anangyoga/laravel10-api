feef5790929459d343b0e4bb352e00cddde9d1c3
anangyoga <aharwinto@gmail.com>
2023-09-23 22:29:24 +0700

    feat: Eloquent Accessort

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Post extends Model
 {
@@ -21,4 +22,19 @@ class Post extends Model
         'title',
         'content',
     ];
+
+
+     /**
+     * image
+     *
+     * @return Attribute
+     */
+
+    //  nama function, dalam hal ini image(), HARUS SESUAI dengan nama field/kolom pada phpmyadmin di tabel posts
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/posts/' . $image),
+        );
+    }
 }
