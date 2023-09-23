f26cf3e842cd11cb2f5d6bae0f8df1b5e0358cbe
anangyoga <aharwinto@gmail.com>
2023-09-23 22:05:29 +0700

    feat: API Resource

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    //define property
+
+    // status rest api
+    public $status; 
+
+    // message
+    public $message;
+
+    // data displayed
+    public $resource;
+
+    // construct menerima data yang diparsing dari controller
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $status
+     * @param  mixed $message
+     * @param  mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource
+        ];
+    }
+}
